0b879125eff891bfdcc4cd8914e06e2c7eb2ef81
Harsh Patel <harsh@krishaweb.com>
2026-09-01 11:03:18 +0530

    Fix css and js issue

--- assets/layout-2/js/stlms-setting.js
+++ assets/layout-2/js/stlms-setting.js
@@ -23,14 +23,14 @@ jQuery(function($) {
 function openTab(evt, tabName) {
   // Hide all tab panes
   const tabcontent = document.getElementsByClassName("tab-pane");
-  for (let i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].classList.remove("active");
+  for (const pane of tabcontent) {
+    pane.classList.remove("active");
   }
 
   // Remove active class from all nav links
   const tablinks = document.getElementsByClassName("nav-link");
-  for (let i = 0; i < tablinks.length; i++) {
-    tablinks[i].classList.remove("active");
+  for (const link of tablinks) {
+    link.classList.remove("active");
   }
 
   // Show current tab pane and mark as active
